330ffc86fd199cf43f63557dd23882dcfdca5d59
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-11-07 12:06:09 -0300

    Create desafio01.js

---
+++ Curso-JS/inicio/desafio01.js
@@ -0,0 +1,18 @@
+// Missão -> exibir em duas colunas os numeros pares e numeros impares
+console.log("Numeros Impáres")
+    {
+        console.log("1");
+        console.log("3");
+        console.log("5");
+        console.log("7");
+        console.log("9");
+    }
+
+console.log("Numeros Pares")
+    {
+        console.log("2");
+        console.log("4");
+        console.log("6");
+        console.log("8");
+        console.log("10");
+    }
