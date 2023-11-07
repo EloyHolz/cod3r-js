82e59a57999228923ce0f8f700e2611fab2710ae
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-11-06 21:27:34 -0300

    Update hello.py

--- CS50P/week0/hello.py
+++ CS50P/week0/hello.py
@@ -1,3 +1,15 @@
-print("Qual o seu nome? ")
-input()
-print("Hello, Wolrd")
+# Perguntando ao usu o seu nome 
+name = input("Qual o seu nome? ")
+
+# Dizendo olá para o usuário 
+print(f"Olá, {name}"Eloy) 
+
+# Outra forma de fazer ficar na mesma str
+    # print ("olá, ", end="")
+    # print (name)
+
+    # para gerar espaçãmento entre argumetnos na mesma str
+    # print ("hello,", name, sep=" ")
+
+    # Dizer o nome do usuário 
+    # print ("hello," , name)
