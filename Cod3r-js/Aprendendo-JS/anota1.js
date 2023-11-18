b1b4abe7523e7e849873432009dbc0b3a9eb791a
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-11-18 11:39:16 -0300

    Update anota1.js

--- Cod3r-js/Aprendendo-JS/anota1.js
+++ Cod3r-js/Aprendendo-JS/anota1.js
@@ -1,1 +1,22 @@
-console.log("alguma coisa")
+const escola = "Cod3r"
+
+console.log(escola.charAt(0))
+console.log(escola.charAt(1))
+console.log(escola.charAt(2))
+console.log(escola.charAt(3))
+console.log(escola.charAt(4))
+console.log(escola.charAt(5))
+
+// Para saber o indice associado ao caractere do Unicode
+console.log(escola.charCodeAt(3)) 
+
+// Função bastante usada para falar o que vem apos o indice selecionado - substring
+console.log(escola.substring (1))
+// va do 0 ate o indice final (o 3) sem mostar o 3
+console.log(escola.substring(0, 3))
+
+// Pode concatenar - juntar com outras palavras
+console.log('Escola '.concat(escola).concat("!"))
+
+// replace - troca um caractere por outro
+console.log(escola.replace(3,"e"))
