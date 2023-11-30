237bf1cc641dd91f48038738198988ea0abf3d00
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-11-30 18:43:20 -0300

    Aprendendo sobre condições em JS

---
+++ JS-Cev/mod-4/aula8.js
@@ -0,0 +1,8 @@
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel>60) {
+    console.log('Você ultrapassou o Limite de Velocidade. MULTADO!')
+} else {
+    console.log('Você está no Limite de velociade. Continue Assim')
+}
+console.log('Dirija sempre usando o Cinto de Segurança')
---
+++ JS-Cev/mod-4/aula9.js
@@ -0,0 +1,8 @@
+var pais = 'Brasil'
+
+console.log(`Se você nasceu no(a) ${pais}, entao: `)
+if (pais = 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
