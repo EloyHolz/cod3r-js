5867f8dc373886941f7fa3c9c7ec251e2bb6c01b
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-12-04 18:42:05 -0300

    Update aula14.js

--- JS-Cev/mod-4/aula14.js
+++ JS-Cev/mod-4/aula14.js
@@ -9,7 +9,29 @@ var diaSem = agora.getDay()
 4 = Quinta
 5 = Sexta 
 6 = Sábado
-7 = Domingo
 */ 
 
-console.log(diaSem)
+
+switch (diaSem) {
+    case 0:
+        console.log("Hoje é Domingo!")
+        break
+    case 1:
+        console.log("Hoje é Segunda!")
+        break
+    case 2:
+        console.log("Hoje é Terça!")
+        break
+    case 3:
+        console.log("Hoje é Quarta!")
+        break
+    case 4:
+        console.log("Hoje é Quinta!")
+        break
+    case 5:
+        console.log("Hoje é Sexta!")
+        break
+    case 6:
+        console.log("Hoje é Sábado!")
+        break
+}
