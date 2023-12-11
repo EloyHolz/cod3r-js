1ca57aa60c1fd847061b667603b031d33aecd8df
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-12-11 18:35:53 -0300

    att atv

--- JS-Cev/mod-5/exercicios/aula-19/script1.js
+++ JS-Cev/mod-5/exercicios/aula-19/script1.js
@@ -1,13 +1,13 @@
 function contar(){
-    let  ini = window.document.querySelector('div#iinico')
-    let  pas = window.document.querySelector("div#ipasso")
-    let fim = window.document.querySelector("div#ifim")
-    let  res = window.document.querySelector("div#resultado")
 
-    if(ini.value.length==0 || fim.value.length==0 || pas.value.length==0){
-        window.alert("[Erro] Faltam dados")
-    } else{
-        window.alert("Tudo ok!")
+    var ini = document.getElementById('iinicio')
+    var passo = document.getElementById('ipasso')
+    var fim = document.getElementById('ifim')
+    var res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('[ERRO] Faltam Elementos')
+    } else {
+        res.innerHTML = 'contando: '
     }
-
 }
