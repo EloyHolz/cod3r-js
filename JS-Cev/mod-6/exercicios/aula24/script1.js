1b158565efa34e9e4098a9136efedc183f777a5c
Eloy Holzgrefe <eloyneto61@gmail.com>
2023-12-19 20:14:55 -0300

    Finalizando atv

--- JS-Cev/mod-6/exercicios/aula24/script1.js
+++ JS-Cev/mod-6/exercicios/aula24/script1.js
@@ -21,6 +21,7 @@ function inLista(n, l){
 }
 
 function adicionar(){
+
    if (isNumero(num.value) && !inLista(num.value, valores)){
         // Para adiconar valores -> mas ainda nao aparecem
         valores.push(Number(num.value))
@@ -29,6 +30,7 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `Valor ${num.value} foi adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
 
    } else {
     window.alert ('[ERRO] Valor inválido ou já adicionado;')
@@ -41,5 +43,31 @@ num.focus()
 
 
 function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione algum valor antes de finalizar')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
 
+        for(let pos in valores){
+
+            soma += valores[pos]
+
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos <strong>${total}</strong> números cadastrados! </p>` 
+        res.innerHTML += `<p>O Maior valor informado foi <strong>${maior}</strong>;</p>`
+        res.innerHTML += `<p>O Menor valor informado foi <strong>${menor}</strong>;</p>`
+        res.innerHTML += `<p>Somando todos os valores temos como resultado: <strong>${soma}</strong></p>`
+        res.innerHTML += `<p>A média entre todos os valores é: <strong>${media}</strong></p>`
+    }
 } 
